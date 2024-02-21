44ee0594afd538a13a893daf9a9539349a0449cb
Junil Ko <junil@mocapot.com>
2024-02-21 21:43:19 +0900

    ArrayList And Hashtable 구현 완료

---
+++ Assets/Scripts/ArrayListAndHashtable/SubtitleCellController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SubtitleCellController : MonoBehaviour
+{
+    public TMP_Text Title;
+    public TMP_Text SubTitle;
+}
